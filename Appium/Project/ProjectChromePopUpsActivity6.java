5161fd0219e9a724d4b5d1569d2955c59da994fd
Sairam3162 <81429263+Sairam3162@users.noreply.github.com>
2025-02-21 13:11:15 +0530

    Update and rename Activity6.java to ProjectChromePopUpsActivity6.java

--- Appium/Project/ProjectChromePopUpsActivity6.java
+++ Appium/Project/ProjectChromePopUpsActivity6.java
@@ -17,7 +17,7 @@ import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
-public class Activity6 {
+public class ProjectChromePopUpsActivity6 extends ActionBase {
 
     AndroidDriver driver;
     WebDriverWait wait;
